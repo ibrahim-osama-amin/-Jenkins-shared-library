918aa4884fa038f28d432ff443d7fc05441a129d
Ibrahim Osama <154071047+ibrahim-osama-amin@users.noreply.github.com>
2024-07-13 20:36:51 +0300

    Update dockerLogin.groovy

--- vars/dockerLogin.groovy
+++ vars/dockerLogin.groovy
@@ -1,10 +1,5 @@
 #!/usr/bin/env groovy
 
-//import com.example.Docker
-
-//def call() {
-//    return new Docker(this).dockerLogin()
-//}
 def dockerlogin(){
         sh "echo $PASS | docker login -u $USER --password-stdin"
         sh 'docker push nanajanashia/demo-app:jma-2.0'
